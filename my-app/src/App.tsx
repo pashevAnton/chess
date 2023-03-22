26b8ffa0aa3cf2d960157addc51c221ee6fab780
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-03-22 04:12:19 +0300

    Add beaten figures

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,13 +2,20 @@ import React, {useEffect, useState} from 'react';
 import BoardComponent from "./components/BoardComponent";
 import "./App.css"
 import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
+import LostFigures from "./components/LostFigures";
 
 function App() {
 
     const [board, setBoard] = useState(new Board())
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
 
     useEffect(() => {
         restart()
+        setCurrentPlayer(whitePlayer)
     },[])
 
     function restart (){
@@ -17,12 +24,29 @@ function App() {
         newBoard.addFigures()
         setBoard(newBoard)
     }
+
+    function swapPlayer(){
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+    }
+
     return (
         <div className="app">
             <BoardComponent
                 board={board}
                 setBoard={setBoard}
+                currentPlayer={currentPlayer}
+                swapPlayer={swapPlayer}
             />
+            <div>
+                <LostFigures
+                    title="Чёрные фигуры"
+                    figures={board.lostBlackFigures}
+                />
+                <LostFigures
+                    title="Белые фигуры"
+                    figures={board.lostWhiteFigures}
+                />
+            </div>
         </div>
     );
 }
